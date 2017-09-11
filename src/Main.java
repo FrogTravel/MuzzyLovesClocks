63e0b2d074cfb65154a4a0977d26d31db4faf1ec
FrogTravel <e.levchenko@innopolis.ru>
2017-09-11 15:12:18 +0300

    Final version (OMG it's been half of a year)

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
 
         Scanner scanner = null;
         try {
@@ -17,60 +15,48 @@ public class Main {
             e.printStackTrace();
         }
 
-
-        int maxValue = 0;
-        int maxCost = 0;
-
-        String[] allValues = scanner.nextLine().split(" ");
+        String[] allValues = scanner.nextLine().split(" ");//Read line of data
         int totalWeight = getWeight(scanner.nextLine());
-        int[] values = new int[allValues.length / 2];
-        int[] weights = new int[allValues.length / 2];
+        int[] values = new int[allValues.length / 2];//half of first line are values
+        int[] weights = new int[allValues.length / 2];//half of second line are weight
 
+        //Parse data
         int index = 0;
         for (int i = 0; i < allValues.length; i += 2) {
             values[index] = getValue(allValues[i]);
             weights[index] = getWeight(allValues[i + 1]);
 
-            System.out.println("Value: " + values[index] + "\nWeight: " + weights[index]);
             index++;
         }
 
+        writeFile(findOptimal(totalWeight, values, weights));
+    }
 
-        int[][] table = new int[totalWeight + 1][values.length + 1];
+    /**
+     * Transform clock format to minutes (so multiple to 60)
+     * @param s data
+     * @return transformed data
+     */
+    private static int getValue(String s) {
+        String[] tokens = s.split("[:]");
+        return Integer.valueOf(tokens[0]) * 60 + Integer.valueOf(tokens[1]);
+    }
 
-        for (int i = 1; i <= totalWeight; i++) {
-            for (int j = 1; j <= values.length; j++) {
-                int number = 0;
-                if ((i - weights[j-1] >= 0)&&(j-1 >= 0)) {
-                    number = table[i - weights[j-1]][j - 1];
-                }
-                table[i][j] = Math.max(table[i][j-1], values[j - 1] + number);
+    /**
+     * Multiple Muzzy's money to 100 so it is more convenient to use
+     * @param s data
+     * @return transformed data
+     */
+    private static int getWeight(String s) {
+        double number = Double.valueOf(s) * 100;
+        return (int) number;
+    }
 
-                  // showTable(table);
-
-//                try{
-//                    table[i][j] = Math.max(table[i][j-1], values[j] + table[i-weights[j]][j-1]);
-//                }catch (ArrayIndexOutOfBoundsException e){
-//
-//                }
-//                int first = 0;
-//                int second = 0;
-//                if(j-1 >= 0){
-//                    first = table[i][j-1];
-//                    if(i-weights[j] >= 0)
-//                        second = table[i-weights[j]][j-1];
-//                }
-//
-//                table[i][j] = Math.max(first, values[j] + second);
-//
-//
-//                //showTable(table);
-            }
-        }
-
-
-        showTable(table);
-
+    /**
+     * Write answer to "output.txt"
+     * @param element to write
+     */
+    private static void writeFile(int element){
         PrintWriter printWriter = null;
         try {
             printWriter = new PrintWriter("output.txt", "UTF-8");
@@ -80,29 +66,33 @@ public class Main {
             e.printStackTrace();
         }
 
-
-        printWriter.print(table[totalWeight][values.length]);
+        printWriter.print(element);
         printWriter.close();
     }
 
-    private static void showTable(int[][] table) {
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table[0].length; j++) {
-                System.out.print(table[i][j] + " ");
+    /**
+     * Classic 1/0 knapsack problem. Find maximum value that we can take with current maximum cost(weight)
+     * @param totalWeight maximum weight
+     * @param values of elements
+     * @param weights of elements
+     * @return right bottom element
+     */
+    private static int findOptimal(int totalWeight, int[] values, int[] weights){
+        int[][] table = new int[totalWeight + 1][values.length + 1];
+
+        for (int j = 1; j <= values.length; j++) {
+            for (int i = 1; i <= totalWeight; i++) {
+                if(weights[j-1] > i){
+                    table[i][j] = table[i][j-1];
+                }else {
+                    int number = 0;
+                    if (i - weights[j - 1] >= 0) {
+                        number = table[i - weights[j - 1]][j - 1];
+                    }
+                    table[i][j] = Math.max(table[i][j - 1], values[j - 1] + number);
+                }
             }
-            System.out.println();
         }
-
-        System.out.println("----------------------------------");
-    }
-
-    private static int getValue(String s) {
-        String[] tokens = s.split("[:]");
-        return Integer.valueOf(tokens[0]) * 60 + Integer.valueOf(tokens[1]);
-    }
-
-    private static int getWeight(String s) {
-        double number = Double.valueOf(s) * 100;
-        return (int) number;
+        return table[totalWeight][values.length];
     }
 }
